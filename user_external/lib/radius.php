a5f835a130b686b2ca28ce67b940dd177125bbbf
Phinitnan Chanasabaeng <phinitnan_c@xtony.us>
2016-02-23 11:46:37 +0700

    Clean up and first commit

--- user_external/lib/radius.php
+++ user_external/lib/radius.php
@@ -5,43 +5,42 @@
  * later.
  */
 
-class OC_User_RADIUS extends OC_User_Backend{
+class OC_User_RADIUS extends \OCA\user_external\Base {
 	private $host;
 	private $realm;
 	private $secret;
 
-	const loginError='NT_STATUS_LOGON_FAILURE';
-	
+	const backend='radius';
+
 	public function __construct($host, $realm, $secret) {
-		$this->host=$host;
-		$this->realm=$realm;
-		$this->secret=$secret;
+		$this->host = $host;
+		$this->realm = $realm;
+		$this->secret = $secret;
+		$backend = backend.'://';
+		$backend .= $host;
+		$backend .= ($realm == ""?"":$realm."@");
+		parent::__construct($backend);
 	}
-	
+
 	/**
 	 * @brief Check if the password is correct
 	 * @param $uid The username
 	 * @param $password The password
-	 * @returns true/false
 	 *
-	 * Check if the password is correct without logging in the user
+	 * @returns true/false
 	 */
 	public function checkPassword($uid, $password) {
 		require_once('apps/user_external/lib/radius.class.php');
 
 		$radius = new Radius($this->host, $this->secret);
-		
-		if(! $radius->AccessRequest($uid."@".$this->realm, $password)  )
+		$user = $uid.($this->realm == ""?"":"@".$this->realm);
+
+		if(! $radius->AccessRequest($user, $password) )
 		{
 			return false;
-		}else{
+		} else {
+			$this->storeUser($uid);
 			return $uid;
 		}
-
-
-	}
-	
-	public function userExists($uid) {
-		return true;
 	}
 }
